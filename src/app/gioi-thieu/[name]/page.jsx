4c48cf8ea85164bc98ce5281afed5f52f5cfe5ea
Xuan Truong Ubuntu <tranxuantruong420@gmail.com>
2024-09-03 20:07:21 +0700

    [UPDATE] - update chi-tiet-thanh-vien full info

--- src/app/gioi-thieu/[name]/page.jsx
+++ src/app/gioi-thieu/[name]/page.jsx
@@ -9,9 +9,7 @@ import InfoMembers from "../../../data/Members/dataMembers.json";
 const InfoMemberPage = ({ params }) => {
   const [member, setMember] = useState({});
   useEffect(() => {
-    const newMember = InfoMembers.find(
-      (member) => member.id === params.name
-    );
+    const newMember = InfoMembers.find((member) => member.id === params.name);
 
     if (newMember) {
       setMember(newMember);
@@ -23,74 +21,124 @@ const InfoMemberPage = ({ params }) => {
   return (
     <>
       {member ? (
-        <Container className="mt-5">
+        <Container className="py-5">
           <Row>
-            <h1 className="text-center mb-4 text-4xl font-bold">{member.first_name} {member.last_name}</h1>
+            <h1 className="text-center mb-4 text-4xl font-bold">
+              {member.first_name} {member.last_name}
+            </h1>
             <Col lg={3} className="mx-auto">
-              <Row>
-                <Image
-                  src={`/images/members/${member.first_name} ${member.last_name}.webp`}
-                  alt={`Avatar ${member.first_name} ${member.last_name}`}
-                  width={300}
-                  height={400}
-                  style={{ borderRadius: "25px" }}
-                ></Image>
-              </Row>
+              <img
+                className="rounded"
+                src={member.image}
+                alt={`Avatar ${member.first_name} ${member.last_name}`}
+                style={{
+                  width: "100%",
+                  height: 350,
+                  objectFit: "cover",
+                }}
+              ></img>
             </Col>
-            <Col lg={4}>
-              <div
-                id="noibo"
-                className="name-posts text-center pt-2 pb-2 fw-bold"
-                style={{ backgroundColor: "#003056", color: "white" }}
-              >
-                Thông tin
-              </div>
 
-              <div className="d-flex">
-                <div>
-                  <i className="bi bi-person-fill fs-2"></i>
-                </div>
-                <div className="ms-3 mt-2 fs-5">
-                  <span className="fw-bold">Họ và tên:</span> {member.first_name} {member.last_name}
-                  <br />
-                  <span className="fw-bold">Giới tính:</span> {member.gender}
-                  <br />
-                  <span className="fw-bold">Ngày sinh:</span> {member.birth_date}
-                  <br />
-                  <span className="fw-bold">Lớp:</span> {member.class}
-                  <br />
-                  <span className="fw-bold">Thế hệ:</span> {member.generation}
-                </div>
-              </div>
-            </Col>
-            <Col lg={5}>
-              {" "}
-              <div
-                id="noibo"
-                className="name-posts text-center pt-2 pb-2 imagePost-radius fw-bold"
-                style={{ backgroundColor: "#003056", color: "white" }}
-              >
-                Quá trình hoạt động
-              </div>
-              <div className="d-flex">
-                <div>
-                  {/* Icon */}
-                  <i className="bi bi-server fs-2"></i>
-                </div>
-                <div className="ms-3 mt-2 fs-5">
-                  {/* Theo từng năm */}
-                  <span className="fw-bold">2023:</span>
-                  <br />
-                  <div className="ms-4">
-                    <p>
-                      <i class="bi bi-dot"></i>Tham gia CLB Hỗ trợ Lập trình
-                    </p>
-                    <p>
-                      <i class="bi bi-dot"></i>Tham gia ban chuyên môn
-                    </p>
-                  </div>
-                </div>
-              </div>
+            <Col className="infoMember ms-2">
+              <Row>
+                <Col lg={5}>
+                  <Row
+                    className="item-title py-2 px-2 rounded-lg fw-bold mb-3"
+                    style={{
+                      backgroundColor: "#003056",
+                      color: "white",
+                    }}
+                  >
+                    <p className="text-center">Thông tin</p>
+                  </Row>
+                  <Row>
+                    <div className="fs-5 item-info">
+                      <span className="fw-bold">Họ và tên: </span>
+                      {member.first_name} {member.last_name}
+                      <br />
+                      <span className="fw-bold">Giới tính: </span>{" "}
+                      {member.gender}
+                      <br />
+                      <span className="fw-bold">Ngày sinh: </span>
+                      {member.birth_date}
+                      <br />
+                      <span className="fw-bold">Lớp: </span> {member.class}
+                      <br />
+                      <span className="fw-bold">Thế hệ: </span>
+                      {member.generation}
+                    </div>
+                  </Row>
+                </Col>
+                <Col lg={5}>
+                  <Row
+                    className="item-title py-2 px-2 rounded-lg fw-bold mb-3"
+                    style={{
+                      backgroundColor: "#003056",
+                      color: "white",
+                    }}
+                  >
+                    <p className="text-center">Thành tích</p>
+                  </Row>
+                  <Row>
+                    {/* Theo từng năm */}
+                    {InfoMembers.filter((member) =>
+                      member.id === params.name ? member : null
+                    ).map((member, index) => {
+                      return (
+                        <>
+                          {member.achievements.map((achievement, index) => {
+                            return (
+                              <>
+                                <span className="fs-4 fw-bold my-1">
+                                  {achievement.year}:
+                                </span>
+                                <br />
+                                {achievement.names.map((name, index) => {
+                                  return (
+                                    <>
+                                      <div className="ms-4">
+                                        <p>
+                                          <i className="bi bi-dot"></i>
+                                          {name}
+                                        </p>
+                                      </div>
+                                    </>
+                                  );
+                                })}
+                              </>
+                            );
+                          })}
+                        </>
+                      );
+                    })}
+                  </Row>
+                </Col>
+              </Row>
+
+              <Row className="mt-5">
+                <Row
+                  className="item-title py-2 px-2 rounded-lg fw-bold mb-3"
+                  style={{
+                    backgroundColor: "#003056",
+                    color: "white",
+                  }}
+                >
+                  <p className="text-center">Quá trình hoạt động</p>
+                </Row>
+                <Row>
+                  {InfoMembers.filter((member) =>
+                    member.id === params.name ? member : null
+                  ).map((member, index) => {
+                    return (
+                      <>
+                        <MemberPositions
+                          positions={member.position}
+                        ></MemberPositions>
+                      </>
+                    );
+                  })}
+                </Row>
+              </Row>
             </Col>
           </Row>
         </Container>
@@ -100,5 +148,34 @@ const InfoMemberPage = ({ params }) => {
     </>
   );
 };
+const MemberPositions = ({ positions = [] }) => {
+  // Tạo một đối tượng để lưu các vị   trí theo từng năm
+  const positionsByYear = {};
 
+  positions.forEach((position) => {
+    const { year, name, description } = position;
+    if (!positionsByYear[year]) {
+      positionsByYear[year] = [];
+    }
+    positionsByYear[year].push(`${name} ${description}`);
+  });
+
+  return (
+    <div>
+      {Object.keys(positionsByYear).map((year) => (
+        <div key={year}>
+          <p className="fs-4 fw-bold my-1">{year}:</p>
+          <ul>
+            {positionsByYear[year].map((positionName, index) => (
+              <li className="ms-4" key={index}>
+                <i className="bi bi-dot"></i>
+                {positionName}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default InfoMemberPage;
